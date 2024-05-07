a0c304d82eabfde8b78993356617b997ab3ef2ed
Jovy <zjjovyy8215@gmail.com>
2024-05-06 22:29:42 -0500

    Some styling

--- src/components/NewSwarmModal.jsx
+++ src/components/NewSwarmModal.jsx
@@ -3,16 +3,16 @@ const NewSwarmModal = (/* props */) => {
     <form class="max-w-sm mx-auto">
       <div class="mb-5">
         <label
-          for="email"
+          for="description"
           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
-          Your email
+          Swarm Description
         </label>
         <input
-          type="email"
-          id="email"
+          type="text"
+          id="description"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="name@flowbite.com"
+          placeholder="Front-end work... etc."
           required
         />
       </div>
@@ -24,7 +24,7 @@ const NewSwarmModal = (/* props */) => {
           Your password
         </label>
         <input
-          type="password"
+          type="number"
           id="password"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           required
