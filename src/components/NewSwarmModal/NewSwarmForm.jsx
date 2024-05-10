5bdc587c751a8e58e758ca7ce7e6fffe416335dd
Jovy <zjjovyy8215@gmail.com>
2024-05-09 21:48:17 -0500

    Fixing front-end

--- src/components/NewSwarmModal/NewSwarmForm.jsx
+++ src/components/NewSwarmModal/NewSwarmForm.jsx
@@ -4,7 +4,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
 const NewSwarmForm = (/* props here */) => {
   return (
-    <div className="flex flex-column justify-center">
+    <div className="h-screen flex items-center justify-center">
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <form className="max-w-lg mx-auto">
           <div className="mb-5">
@@ -17,49 +17,39 @@ const NewSwarmForm = (/* props here */) => {
             <textarea
               rows="4"
               id="description"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Front-end work... etc."
               required
             />
           </div>
-          <div className="mb-5">
-            <label
-              htmlFor="start"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
-              Start Time
-            </label>
-            <TimePicker
-              renderInput={(props) => (
-                <input
-                  {...props}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                />
-              )}
-            />
+          <div className="mb-5 flex">
+            <div className="">
+              <label
+                htmlFor="start"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Start Time
+              </label>
+              <TimePicker/>
+            </div>
+            <div className="ml-2">
+              <label
+                htmlFor="end"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                End Time
+              </label>
+              <TimePicker/>
+            </div>
           </div>
-          <div className="mb-5">
-            <label
-              htmlFor="end"
-              className="block mb-2 text-sm font-medium text-gray-900"
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
-              End Time
-            </label>
-            <TimePicker
-              renderInput={(props) => (
-                <input
-                  {...props}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                />
-              )}
-            />
+              Submit
+            </button>
           </div>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            Submit
-          </button>
         </form>
       </LocalizationProvider>
     </div>
